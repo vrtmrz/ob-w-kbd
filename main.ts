8b578fca768acef1e2f062eca54ce438a48851f5
vorotamoroz <vrtmrz@gmail.com>
2022-05-09 17:19:58 +0900

    initial

--- main.ts
+++ main.ts
@@ -1,137 +1,154 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Plugin, sanitizeHTMLToDom } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface WorkaroundKeyboardSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: WorkaroundKeyboardSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const toggleElement = (element:HTMLElement,visible:boolean)=>{
+	if(visible){
+	element.style.display="";
+	}else{
+		element.style.display="none";
 
-	async onload() {
-		await this.loadSettings();
+	}
+}
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+export default class WorkaroundKeyboardPlugin extends Plugin {
+	settings: WorkaroundKeyboardSettings;
+	textareaWrapper: HTMLElement;
+	textarea: HTMLTextAreaElement;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+	target: Node;
+	targetParent: Node;
+	editor: Editor;
+	popupElement: HTMLElement;
+	popupSpacer: HTMLElement;
+	popupPreview: HTMLElement;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	showTextarea(editor: Editor) {
+		toggleElement(this.textareaWrapper,true);
+		this.popupPreview.textContent = "";
+		toggleElement(this.popupElement,true);
+		this.editor = editor;
+		setTimeout(() =>
+			this.textarea.focus()
+			, 10)
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+	hideTextarea() {
+		toggleElement(this.popupElement,false);
+		toggleElement(this.textareaWrapper,false);
+		this.textarea.value = "";
+		if (this.editor != null) {
+			this.editor.focus();
+		}
+	}
+	previewText() {
+		let content = this.textarea.value ?? "";
+		if (this.popupElement) {
+			this.popupPreview.textContent = content;
+		}
 	}
 
-	onunload() {
+	async onload() {
+		this.popupElement = document.createElement("span");
+		this.popupPreview = document.createElement("span");
+		this.popupSpacer = document.createElement("span");
+		this.popupElement.addClass("workaround-keyboard-popup");
+		this.popupPreview.addClass("preview");
+		this.popupSpacer.addClass("spacer");
 
+
+		this.popupElement.appendChild(this.popupSpacer);
+		this.popupSpacer.appendChild(this.popupPreview);
+
+		document.body.appendChild(this.popupElement);
+		this.target = document.querySelector("div.status-bar");
+		this.targetParent = this.target.parentNode;
+		let el = document.createElement("div");
+
+		this.targetParent.insertBefore(el, this.target);
+		el.addClass("workaround-keyboard");
+		this.textarea = el.createEl("textarea");
+		el.appendChild(this.textarea);
+		this.textareaWrapper = el;
+		this.textarea.addEventListener("blur", () => {
+			this.hideTextarea();
+		})
+		this.textarea.addEventListener("keyup", () => {
+			this.previewText();
+		})
+		this.textarea.addEventListener("keydown", (evt: KeyboardEvent) => {
+			// console.log(evt)
+			this.previewText();
+			let content = this.textarea.value;
+			if (evt.key == "Enter") {
+				if (evt.isComposing) return;
+				evt.preventDefault();
+				this.editor.replaceSelection(content);
+				this.hideTextarea();
+			}
+			if (evt.key == "Escape") {
+				evt.preventDefault();
+				this.hideTextarea();
+			}
+			if (evt.key == "Backspace" && this.textarea.value.length == 0) {
+				evt.preventDefault();
+				this.hideTextarea();
+			}
+		})
+
+		this.registerEvent(this.app.workspace.on("active-leaf-change", () => this.hideTextarea));
+		this.registerEvent(this.app.workspace.on("file-open", () => this.hideTextarea));
+		// window.addEventListener("visibilitychange", this.watchWindowVisiblity);
+		this.hideTextarea();
+
+		await this.loadSettings();
+
+		this.addCommand({
+			id: 'workaround-keyboard-begin',
+			name: 'Open input box',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.showTextarea(editor);
+				const selected = editor.getSelection();
+				this.textarea.value = selected ?? "";
+				const pos = window.getSelection().getRangeAt(0).getBoundingClientRect()
+				this.popupElement.style.left = `0px`;
+				this.popupSpacer.style.textIndent = `${pos.left}px`;
+				this.popupElement.style.top = `${pos.top - 1}px`;
+				this.popupPreview.style.textIndent = "0";
+			}
+		});
+		// this.registerDomEvent(document, 'keydown', (evt: KeyboardEvent) => {
+		// 	//TODO:hook automatic inputarea show
+		// 	if (evt.key == "å") {
+		// 		if (document.activeElement.hasClass("cm-content")) {
+		// 			console.log("Here!")
+		// 		}
+		// 	}
+		// });
+	}
+
+
+	onunload() {
+		if (this.textareaWrapper != null) {
+			this.targetParent.removeChild(this.textareaWrapper);
+			this.textareaWrapper = null;
+			document.body.removeChild(this.popupElement);
+		}
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		// this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
+		// await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
